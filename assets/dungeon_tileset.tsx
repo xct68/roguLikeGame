57422aeedd06ad9bd3827ca08b88f5d215edd5fb
xct68 <mazourine@Hotmail.com>
2024-10-10 22:50:10 +0200

    ?

---
+++ assets/dungeon_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="dungeon_tileset" tilewidth="16" tileheight="16" tilecount="221" columns="17">
+ <image source="../../../../Downloads/16x16 Tileset/16x16 Tileset/TileSet.png" trans="ff00ff" width="272" height="208"/>
+</tileset>
